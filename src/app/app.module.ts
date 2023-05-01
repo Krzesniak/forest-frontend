237d50b973ea0aeb58ec3f17f7085d81bc10e767
Krzesniak <krzesniak.piotr@wp.pl>
2023-05-01 19:26:52 +0200

    skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TerrainBoardComponent } from './boards/terrain-board/terrain-board.component';
+import {HttpClientModule} from "@angular/common/http";
+import { NavbarComponent } from './common/navbar/navbar.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { AppRoutingModule } from './app-routing.module';
+import {RouterOutlet} from "@angular/router";
+import { SimulationComponent } from './simulation/simulation.component';
+import { MapGeneratorComponent } from './map-generator/map-generator.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatInputModule} from "@angular/material/input";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TerrainBoardComponent,
+    NavbarComponent,
+    SimulationComponent,
+    MapGeneratorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatToolbarModule,
+    AppRoutingModule,
+    RouterOutlet,
+    ReactiveFormsModule,
+    MatStepperModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
